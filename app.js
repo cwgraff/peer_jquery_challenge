27f94a3ac55be5d7d8b4db856e0294a0cbff4fe3
CharlieGitDev <charlieangus@live.com>
2015-12-15 17:00:41 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var people = ['charlie', 'chris', 'jeremy', 'natalie', 'scott', 'kenzie', 'eric'];
+var namesCount = 0;
+var fadeToggle = 0;
+
+$(function(){
+	$('#container').on('click', '.remove', function(){
+		if($(this).parent().parent().css('opacity') == 1){
+			$(this).parent().parent().animate({opacity: 0.05});
+		} else {
+			$(this).parent().parent().animate({opacity:1});
+		}
+	});
+
+	$('.create').on('click', function(){
+		if(namesCount > people.length - 1){
+			$('#container').empty();
+			namesCount = 0;
+		} else {
+			$('#container').append('<div id="person' + namesCount + '" class="person"><div class="personname">' + people[namesCount] + '<button class="remove">Fade</button></div></div>');
+			$('#person'+namesCount).hide().slideDown();
+			namesCount++;
+		}
+	});
+});
